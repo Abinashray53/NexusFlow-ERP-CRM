662039441ba0d2dad44f13e9abff128a30ec35c5
Abinash Ray <abinashray53@gmail.com>
2026-09-03 13:50:40 +0530

    Add files via upload

--- types.d.ts
+++ types.d.ts
@@ -1,85 +1,43 @@
 import type { SourceMapSegment } from './sourcemap-segment';
-import type { TraceMap } from './trace-mapping';
 export interface SourceMapV3 {
     file?: string | null;
-    names: string[];
+    names: readonly string[];
     sourceRoot?: string;
-    sources: (string | null)[];
-    sourcesContent?: (string | null)[];
+    sources: readonly (string | null)[];
+    sourcesContent?: readonly (string | null)[];
     version: 3;
+    ignoreList?: readonly number[];
 }
 export interface EncodedSourceMap extends SourceMapV3 {
     mappings: string;
 }
 export interface DecodedSourceMap extends SourceMapV3 {
-    mappings: SourceMapSegment[][];
+    mappings: readonly SourceMapSegment[][];
 }
-export interface Section {
-    offset: {
-        line: number;
-        column: number;
-    };
-    map: EncodedSourceMap | DecodedSourceMap | SectionedSourceMap;
-}
-export interface SectionedSourceMap {
-    file?: string | null;
-    sections: Section[];
-    version: 3;
-}
-export declare type OriginalMapping = {
-    source: string | null;
+export interface Pos {
     line: number;
     column: number;
-    name: string | null;
-};
-export declare type InvalidOriginalMapping = {
-    source: null;
-    line: null;
-    column: null;
-    name: null;
-};
-export declare type GeneratedMapping = {
-    line: number;
-    column: number;
-};
-export declare type InvalidGeneratedMapping = {
-    line: null;
-    column: null;
-};
-export declare type SourceMapInput = string | EncodedSourceMap | DecodedSourceMap | TraceMap;
-export declare type SectionedSourceMapInput = SourceMapInput | SectionedSourceMap;
-export declare type Needle = {
-    line: number;
-    column: number;
-    bias?: 1 | -1;
-};
-export declare type SourceNeedle = {
+}
+export interface OriginalPos extends Pos {
     source: string;
-    line: number;
-    column: number;
-    bias?: 1 | -1;
-};
-export declare type EachMapping = {
-    generatedLine: number;
-    generatedColumn: number;
-    source: null;
-    originalLine: null;
-    originalColumn: null;
-    name: null;
-} | {
-    generatedLine: number;
-    generatedColumn: number;
-    source: string | null;
-    originalLine: number;
-    originalColumn: number;
-    name: string | null;
-};
-export declare abstract class SourceMap {
-    version: SourceMapV3['version'];
-    file: SourceMapV3['file'];
-    names: SourceMapV3['names'];
-    sourceRoot: SourceMapV3['sourceRoot'];
-    sources: SourceMapV3['sources'];
-    sourcesContent: SourceMapV3['sourcesContent'];
-    resolvedSources: SourceMapV3['sources'];
 }
+export interface BindingExpressionRange {
+    start: Pos;
+    expression: string;
+}
+export type Mapping = {
+    generated: Pos;
+    source: undefined;
+    original: undefined;
+    name: undefined;
+} | {
+    generated: Pos;
+    source: string;
+    original: Pos;
+    name: string;
+} | {
+    generated: Pos;
+    source: string;
+    original: Pos;
+    name: undefined;
+};
